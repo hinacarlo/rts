3b55cd14776b91eafd32527aedefab6f67552d6c
hinacarlo <hinacarlo1993@gmail.com>
2023-01-09 10:44:09 +0800

    Using useRefs with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+//import UserSearch from './state/UserSearch';
+/* import Usersearch from './classes/Usersearch'; */
 
 const App = () => {
+  const users = [
+    { name: 'Sarah', age: 20 },
+    { name: 'Alex', age: 20 },
+    { name: 'Carlo', age: 25 },
+  ];
   return (
     <div>
-      <EventComponent />
+      <UserSearch />
     </div>
   );
 };
